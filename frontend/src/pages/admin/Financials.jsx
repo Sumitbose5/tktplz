148f1eb6f2cc61d6412e63ea76f340a0c236a745
Sumitbose05 <bosesomen062@gmail.com>
2025-08-28 07:38:07 +0530

    Removed an unnecessary backend import in frontend causing error in deploying

--- frontend/src/pages/admin/Financials.jsx
+++ frontend/src/pages/admin/Financials.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { users } from "../../../../backend/drizzle/userSchema";
 import { useAuth } from "../../context/AuthContext";
 
 const Financials = () => {
